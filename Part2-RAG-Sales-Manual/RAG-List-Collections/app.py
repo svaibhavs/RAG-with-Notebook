ba7679bdda052a43be3fed4c461514b843548f49
DSpurway <53935437+DSpurway@users.noreply.github.com>
2024-08-22 13:00:24 +0100

    Update app.py

--- Part2-RAG-Sales-Manual/RAG-List-Collections/app.py
+++ Part2-RAG-Sales-Manual/RAG-List-Collections/app.py
@@ -5,7 +5,7 @@ import os
 import logging
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app, origins=["https://rag-webpage-llm-on-techzone.apps.p1296.cecc.ihost.com:8080"]) 
 
 
 app = Flask(__name__)
@@ -15,38 +15,21 @@ if __name__ != '__main__':
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-@app.route('/', methods=["GET", "OPTIONS"])
+@app.route('/')
 def index():
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-    elif request.method == "GET": # The actual request following the preflight
-        content = {}
+    content = {}
         
-        MILVUS_HOST="milvus-service"
-        MILVUS_PORT="19530"
+    MILVUS_HOST="milvus-service"
+    MILVUS_PORT="19530"
         
-        connections.connect(host=MILVUS_HOST, port=MILVUS_PORT)
-        app.logger.info('Connected to Milvus Host '+MILVUS_HOST)
+    connections.connect(host=MILVUS_HOST, port=MILVUS_PORT)
+    app.logger.info('Connected to Milvus Host '+MILVUS_HOST)
         
-        Collections = utility.list_collections()
-        app.logger.info('Found collections:', Collections)
+    Collections = utility.list_collections()
+    app.logger.info('Found collections:', Collections)
         
-        content['result'] = Collections   
-        return _corsify_actual_response(content)
-    else:
-        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
-
-def _build_cors_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "https://rag-webpage-llm-on-techzone.apps.p1296.cecc.ihost.com")
-    response.headers.add('Access-Control-Allow-Headers', "https://rag-webpage-llm-on-techzone.apps.p1296.cecc.ihost.com")
-    response.headers.add('Access-Control-Allow-Methods', "https://rag-webpage-llm-on-techzone.apps.p1296.cecc.ihost.com")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "https://rag-webpage-llm-on-techzone.apps.p1296.cecc.ihost.com")
-    return response
-
+    content['result'] = Collections  
+    
 @app.route('/healthz')
 # Added healthcheck endpoint
 def healthz():
