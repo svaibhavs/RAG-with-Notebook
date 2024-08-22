1d2b8affe6fb2953943a49daf882cc1d19af1fea
DSpurway <53935437+DSpurway@users.noreply.github.com>
2024-08-22 11:55:21 +0100

    Update app.py

--- Part2-RAG-Sales-Manual/RAG-List-Collections/app.py
+++ Part2-RAG-Sales-Manual/RAG-List-Collections/app.py
@@ -1,5 +1,5 @@
 from pymilvus import connections, utility
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
 import os
 import logging
@@ -15,21 +15,37 @@ if __name__ != '__main__':
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-@app.route('/')
+@app.route('/', methods=["POST", "OPTIONS"])
 def index():
-    content = {}
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "POST": # The actual request following the preflight
+        content = {}
         
-    MILVUS_HOST="milvus-service"
-    MILVUS_PORT="19530"
+        MILVUS_HOST="milvus-service"
+        MILVUS_PORT="19530"
+        
+        connections.connect(host=MILVUS_HOST, port=MILVUS_PORT)
+        app.logger.info('Connected to Milvus Host '+MILVUS_HOST)
+        
+        Collections = utility.list_collections()
+        app.logger.info('Found collections:', Collections)
+        
+        content['result'] = Collections   
+        return _corsify_actual_response(content)
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
 
-    connections.connect(host=MILVUS_HOST, port=MILVUS_PORT)
-    app.logger.info('Connected to Milvus Host '+MILVUS_HOST)
-  
-    Collections = utility.list_collections()
-    app.logger.info('Found collections:', Collections)
-        
-    content['result'] = Collections   
-    return content
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 @app.route('/healthz')
 # Added healthcheck endpoint
