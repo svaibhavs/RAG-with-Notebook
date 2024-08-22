7385e9a97cceca731ad27d8c8c4d0b6b9679f00e
DSpurway <53935437+DSpurway@users.noreply.github.com>
2024-08-22 13:41:28 +0100

    Update app.py

--- Part2-RAG-Sales-Manual/RAG-Webpage/app.py
+++ Part2-RAG-Sales-Manual/RAG-Webpage/app.py
@@ -1,28 +1,12 @@
 ## Assisted by WCA@IBM
 ## Latest GenAI contribution: ibm/granite-20b-code-instruct-v2from flask import Flask, render_template
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "OPTIONS"])
+@app.route("/")
 def index():
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-    elif request.method == "GET":
-        return render_template('index.html')
-    else:
-        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
-
-def _build_cors_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+    return render_template('index.html')
   
 @app.route('/healthz')
 # Added healthcheck endpoint
