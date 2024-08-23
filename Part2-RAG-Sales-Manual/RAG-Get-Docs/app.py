6c085b13e169dcdc2cffeae4df86d2919d4bad9f
DSpurway <53935437+DSpurway@users.noreply.github.com>
2024-08-23 15:30:16 +0100

    Update app.py

--- Part2-RAG-Sales-Manual/RAG-Get-Docs/app.py
+++ Part2-RAG-Sales-Manual/RAG-Get-Docs/app.py
@@ -2,8 +2,12 @@ from pymilvus import connections, utility
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Milvus
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import os
 import logging
+
+app = Flask(__name__)
+CORS(app, origins=["https://rag-webpage-llm-on-techzone.apps.p1296.cecc.ihost.com"]) 
 
 app = Flask(__name__)
 
@@ -13,12 +17,16 @@ if __name__ != '__main__':
     app.logger.setLevel(gunicorn_logger.level)
 
 @app.route('/')
+@cross_origin() # allow all origins all methods.
 def index():
     content = {}
 
-    if request.args.get('Server_Name'):
+    if request.args.get('Server_Name','Questions'):
         Server_Name = request.args.get('Server_Name')
         app.logger.info('Found Server_Name '+Server_Name)
+
+        Questions = request.args.get('Questions')
+        app.logger.info('Found Questions '+Questions)
         
         MILVUS_HOST="milvus-service"
         MILVUS_PORT="19530"
@@ -36,15 +44,15 @@ def index():
 
         FNAME = Server_Name+".pdf"
 
-        questions = ["How many dual-chip processor modules in the server?", "How Power10 processors in the server?", "What speed in GHz are the processors in the server?"]
-        question = questions[0]
+#        questions = ["How many dual-chip processor modules in the server?", "How Power10 processors in the server?", "What speed in GHz are the processors in the server?"]
+        question = Questions[0]
 
         docs = vector_store.similarity_search_with_score(question, k=3, expr="source == '"+FNAME+"'")
         app.logger.info('Got docs from vector store')
         
         content['result'] = "Success", str(docs)
     else:
-        content ['result'] = "Server Name Missing"
+        content ['result'] = "Server Name or Questions Missing"
         
     return content
 
