47ee80426ea563c30b3f7d107aa110dce2975dbc
DSpurway <53935437+DSpurway@users.noreply.github.com>
2024-09-02 12:56:25 +0100

    Create app.py

---
+++ Part2-RAG-Sales-Manual/RAG-Prompt-LLM/app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import logging
+import requests
+
+app = Flask(__name__)
+CORS(app, origins=["https://rag-webpage-llm-on-techzone.apps.p1296.cecc.ihost.com"]) 
+
+app = Flask(__name__)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+@app.route('/')
+@cross_origin() # allow all origins all methods.
+def index():
+  content = {}
+
+  LLAMA_HOST="llama-service"
+  LLAMA_PORT="8080"
+  
+  if request.args.get('Prompt'):
+    Prompt = request.args.get('Prompt')
+    app.logger.info('Found Prompt '+Prompt)
+    
+    json_data = {
+      'prompt': Prompt,
+      'temperature': 0.1,
+      'n_predict': 100,
+      'stream': False,
+    }
+    app.logger.info('Sending requesto LLM with this JSON data: '+json_data)
+    
+    res = requests.post(f'http://{LLAMA_HOST}:{LLAMA_PORT}/completion', json=json_data)
+    
+    res.json()['content']
+    
+    content['result'] = "Success"
+  else:
+    content ['result'] = "Prompt Missing"
+  
+  app.logger.info('Returning '+str(content))
+  return content
+
+@app.route('/healthz')
+# Added healthcheck endpoint
+def healthz():
+    return "ok"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
